afd4a07828d1f5d550a125d3c56a034907ee0a5c
Vinicius Higa <vinicius.hiroshi.higa@outlook.com>
2022-06-22 19:34:51 -0300

    Transform split into a vector of strings

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,10 +1,12 @@
 pub fn tokenize(line_content: &str) {
-    let tokens = line_content.split(" ");
+    let tokens: Vec<String> = line_content.split(" ").map(|s| s.to_string()).collect();
     let operation_queue = ["*", "/", "+", "-"];
     let mut currentIndex = 0;
 
     for operation in operation_queue {
-        
+        for index in 0 .. tokens.len() {
+            print!("{}", index);
+        }
     }
 
     // std::deque<std::string> operationQueue = { "*", "/", "+", "-" }
