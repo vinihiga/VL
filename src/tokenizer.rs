3ec20755d2fd924a071059d514bab91f1a5906b8
Vinicius Higa <vinicius.hiroshi.higa@outlook.com>
2022-06-23 19:19:53 -0300

    Simple lexer, synthatic part, semantic part done

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,23 +1,62 @@
 pub fn tokenize(line_content: &str) {
-    let tokens: Vec<String> = line_content.split(" ").map(|s| s.to_string()).collect();
+    let mut tokens: Vec<String> = line_content.split(" ").map(|s| s.to_string()).collect();
     let operation_queue = ["*", "/", "+", "-"];
-    let mut currentIndex = 0;
 
     for operation in operation_queue {
-        for index in 0 .. tokens.len() {
-            print!("{}", index);
+        if tokens.len() >= 3 && tokens[1] == operation {
+            while tokens.len() >= 3 {
+                let new_token = join_variables(
+                    tokens[0].clone(),
+                    tokens[2].clone(),
+                    tokens[1].clone());
+
+                tokens.remove(0);
+                tokens.remove(0);
+                tokens.remove(0);
+
+                if tokens.len() > 0 {
+                    tokens.remove(0);
+
+                    if new_token.parse::<i32>().unwrap() >= 0 {
+                        tokens.push("+".to_string());
+                    } else {
+                        tokens.push("-".to_string());
+                    }
+                }
+
+                tokens.push(new_token);
+
+                println!("{}", tokens.join(" "));
+            }
         }
     }
+}
 
-    // std::deque<std::string> operationQueue = { "*", "/", "+", "-" }
-    // int previousIndex = 0;
+fn join_variables(x1: String, x2: String, operator: String) -> String {
+    let mut result: String = "\0".to_string();
 
-    // for (std::string token : tokens) {    
+    if operator == "+" {
+        let value1 = x1.parse::<i32>().unwrap();
+        let value2 = x2.parse::<i32>().unwrap();
+        result = (value1 + value2).to_string();
+    } else if operator == "-" {
+        let value1 = x1.parse::<i32>().unwrap();
+        let value2 = x2.parse::<i32>().unwrap();
+        result = (value1 - value2).to_string();
+    } else if operator == "*" {
+        let value1 = x1.parse::<i32>().unwrap();
+        let value2 = x2.parse::<i32>().unwrap();
+        result = (value1 * value2).to_string();
+    } else if operator == "/" {
+        let value1 = x1.parse::<i32>().unwrap();
+        let value2 = x2.parse::<i32>().unwrap();
 
-    //     std::find(operationQueue.begin(), operationQueue.end(), token)
+        if value2 == 0 {
+            panic!("Division by zero!");
+        }
 
-    //     std::cout << "Output: " << token.c_str() << std::endl;
-    // }
+        result = (value1 / value2).to_string();
+    }
 
-    // std::cout << "Loaded symbols" << std::endl;
+    return result;
 }
